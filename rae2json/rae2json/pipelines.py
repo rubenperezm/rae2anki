45ab4708ac99d72a807554bfd3c6ed53e8a38883
rubenperezm <rubenpermerc@gmail.com>
2026-05-13 17:58:40 +0200

    feat: adapt crawler process to scrapy 2.15

--- rae2json/rae2json/pipelines.py
+++ rae2json/rae2json/pipelines.py
@@ -9,15 +9,23 @@ import json
 from itemadapter import ItemAdapter
 
 class Rae2JsonPipeline:
-    def open_spider(self, spider):
-        output_file = spider.settings.get('OUTPUT_FILE', 'output.json')
-        self.file = open(output_file, 'w', encoding='utf-8')
+    def __init__(self, output_file):
+        self.output_file = output_file
+        self.file = None
         self.data = {}
 
-    def close_spider(self, spider):
+    @classmethod
+    def from_crawler(cls, crawler):
+        output_file = crawler.settings.get('OUTPUT_FILE', 'output.json')
+        return cls(output_file)
+    
+    def open_spider(self):
+        self.file = open(self.output_file, 'w', encoding='utf-8')
+
+    def close_spider(self):
         json.dump(self.data, self.file, ensure_ascii=False)
         self.file.close()
 
-    def process_item(self, item, spider):
+    def process_item(self, item):
         self.data.update(item)
         return item
